c621f0ac956aee03a1f474779e834193ded5dabf
yeomkyeorae <duarufp06@naver.com>
2019-11-28 10:11:52 +0900

    KR | Deploy | Procfile

---
+++ movies/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Genre, Movie, Review, ScoresExpected
+
+
+class GenreSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True, source='user.username')
+    class Meta:
+        model = Review
+        fields = ['id', 'score', 'content', 'user', 'username', 'movie']
+
+
+class MovieSerializers(serializers.ModelSerializer):
+    review_set = ReviewSerializers(many=True)
+    class Meta:
+        model = Movie
+        fields = ['code', 'title', 'poster_url', 'description', 'genres', 'like_users', 'watched_users', 'review_set']
+
+
+class GenreDetailSerializers(serializers.ModelSerializer):
+    movies = MovieSerializers(many=True)
+    class Meta(GenreSerializers.Meta): 
+        fields = GenreSerializers.Meta.fields
+        include = ['movies']
+
+
+class ScoresExpectedSerializers(serializers.ModelSerializer):
+    movie = MovieSerializers()
+    username = serializers.CharField(read_only=True, source='user.username')
+    class Meta:
+        model = ScoresExpected
+        fields = ['score', 'movie', 'user', 'movie', 'username']
+
+# class MovieDetailSerializers(serializers.ModelSerializer):
+#     reviews = ReviewSerializers(many=True)
+#     class Meta:
+#         fields = MovieSerializers.Meta.fields
+#         include = ['reviews']
