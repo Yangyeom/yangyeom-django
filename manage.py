739e39355b455eecca698d9e291943010d6e7b04
yeomkyeorae <duarufp06@naver.com>
2019-11-28 11:19:43 +0900

    production to local

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from decouple import config
 
 def main():
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yangyeomchicken.settings')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yangyeomchicken.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yangyeomchicken.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
